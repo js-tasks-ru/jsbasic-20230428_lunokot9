804461f485ca2f6d802df544ba8fb228165fdda2
lunokot <anna.mikhailova@emailmatrix.ru>
2023-05-03 14:25:04 +0300

    Модуль 1, задачи 1-2-3

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -1,3 +1,9 @@
 function factorial(n) {
-  // ваш код...
+    if (n === 0) return 1;
+  
+    let result = n;
+    for (let i = n - 1; i > 0; i--) {
+        result *= i;
+    }
+    return result;
 }
