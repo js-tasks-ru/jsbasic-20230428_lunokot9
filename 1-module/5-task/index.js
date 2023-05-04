620b631d3c9d084e58c886818e893d5796f9cff4
lunokot <anna.mikhailova@emailmatrix.ru>
2023-05-04 11:32:39 +0300

    Правка для задачи 5 из модуля 1

--- 1-module/5-task/index.js
+++ 1-module/5-task/index.js
@@ -1,5 +1,5 @@
 function truncate(str, maxlength) {
-    if (str.length < maxlength) return str;
+    if (str.length <= maxlength) return str;
     
     return str.substr(0, maxlength - 1) + '…';
 }
