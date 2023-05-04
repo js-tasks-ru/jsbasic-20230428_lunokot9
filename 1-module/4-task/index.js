7c090c87d2959e815102a45c7b7629978f14a4c1
lunokot <anna.mikhailova@emailmatrix.ru>
2023-05-04 14:32:02 +0300

    Правка для задачи 4 из модуля 1

--- 1-module/4-task/index.js
+++ 1-module/4-task/index.js
@@ -1,4 +1,4 @@
 function checkSpam(str) {
     str = str.toLowerCase();
-    return (str.includes('xxx') || str.includes('1xbet') || false);
+    return (str.includes('xxx') || str.includes('1xbet'));
 }
