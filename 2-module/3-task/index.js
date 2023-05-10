e54b7042f071b64577dd7089b2de92063ffd3499
lunokot <anna.mikhailova@emailmatrix.ru>
2023-05-10 14:44:52 +0300

    Модуль 2, задачи 1-3

--- 2-module/3-task/index.js
+++ 2-module/3-task/index.js
@@ -1,5 +1,16 @@
 let calculator = {
-  // ваш код
+    num1: null,
+    num2: null,
+    read: function(a, b) {
+        this.num1 = a;
+        this.num2 = b;
+    },
+    sum: function() {
+        return this.num1 + this.num2;
+    },
+    mul: function() {
+        return this.num1 * this.num2;
+    }
 };
 
 // НЕ УДАЛЯТЬ СТРОКУ, НУЖНА ДЛЯ ПРОВЕРКИ
