874be95feb77db04e035821e567bc9b6fee1ec44
lunokot <anna.mikhailova@emailmatrix.ru>
2023-05-29 15:22:15 +0300

    Модуль 4 и 5

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -1,3 +1,9 @@
 function makeFriendsList(friends) {
-  // ваш код...
+    let friendsListUl = document.createElement('ul');
+
+    friends.forEach(item => {
+        friendsListUl.innerHTML += `<li>${item.firstName} ${item.lastName}</li>`;
+    });
+
+    return friendsListUl;
 }
--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -1,3 +1,15 @@
 function highlight(table) {
-  // ваш код...
+    for (tr of table.tBodies[0].rows) {
+        if (!tr.cells[3].dataset.available) {
+            tr.hidden = true;
+        } else {
+            let trStatusClass = (tr.cells[3].dataset.available === 'true') ? 'available' : 'unavailable';
+            tr.classList.add(trStatusClass);
+        }
+        
+        let trGenderClass = (tr.cells[2].textContent == 'm') ? 'male' : 'female';
+        tr.classList.add(trGenderClass);
+
+        if (Number(tr.cells[1].textContent) < 18) tr.style.textDecoration = 'line-through';
+    }
 }
